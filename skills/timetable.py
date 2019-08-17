f296438e4502807f3a06c4b22c44947921d385b8
Geunwoo Shin <encyphered@gmail.com>
2019-08-17 10:48:29 +0900

    Add shortcut menu to timetable link (#30)
    * Add shortcut menu to timetable link
    
    * Fix PEP8 violation

---
+++ skills/timetable.py
@@ -0,0 +1,25 @@
+#  Copyright 2019 LINE Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+
+from linebot.models import TextSendMessage
+
+from skills import add_skill
+
+
+@add_skill(r'시간표')
+def get_timetable(message):
+    return TextSendMessage(
+        text='시간표는 https://www.pycon.kr/timetable/talks 에서 확인해주세요'
+    )
